9e6eb4e4437fa61f9373144920054d96b821ed94
Color_yr <402067010@qq.com>
2022-04-17 13:24:27 +0800

    up

--- L2d_Desktop/L2d_Desktop/obj/Debug/net6.0-windows/App.g.i.cs
+++ L2d_Desktop/L2d_Desktop/obj/Debug/net6.0-windows/App.g.i.cs
@@ -48,7 +48,7 @@ namespace L2d_Desktop {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.2.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.3.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
@@ -60,7 +60,7 @@ namespace L2d_Desktop {
             
             #line default
             #line hidden
-            System.Uri resourceLocater = new System.Uri("/L2d_Desktop;component/app.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/L2d_Desktop;V1.0.0.0;component/app.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\App.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
@@ -74,7 +74,7 @@ namespace L2d_Desktop {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.2.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.3.0")]
         public static void Main() {
             L2d_Desktop.App app = new L2d_Desktop.App();
             app.InitializeComponent();
